32a799ef0633989e703d8c1097db30daf730329f
Simon Thel <s.thel@tu-braunschweig.de>
2021-04-20 10:26:08 +0200

    avoided pagetranspose

--- assembly.m
+++ assembly.m
@@ -12,26 +12,26 @@ function [FORCE, GKF] = assembly(DISPTD, xPhys, penal, beta, eta, NE, NEQ, NDOF,
     % Interpolation factor
     gamma = reshape((tanh(beta*eta) + tanh(beta*(xPhys.^penal - eta))) / ...
         (tanh(beta * eta) + tanh(beta*(1-eta))),1,1, NE);
-    GDSP_int = pagemtimes(gamma, GDSP);
+    GDSP_int = repmat(gamma, 3, 8, 1).*GDSP;
     % Deformation gradient see eq. 3.5
     d0U = permute(reshape(permute(reshape(pagemtimes(GDSP, gSHPDT), ...
         [3 1 3 8 NE]), [1 3 2 4 5]), [3 3 8 NE]), [1 2 4 3]);
     d0U_int = permute(reshape(permute(reshape(pagemtimes(GDSP_int, gSHPDT), ...
         [3 1 3 8 NE]), [1 3 2 4 5]), [3 3 8 NE]), [1 2 4 3]);
     F_int = d0U_int + repmat(eye(3), 1, 1, NE, 8);
-    FT_int = pagetranspose(F_int);
+    FT_int = permute(F_int, [2 1 3 4]);
     C_int = pagemtimes(FT_int, F_int);
     % Lagrangian strain eq. 3.10
     E_int = 0.5*(C_int-repmat(eye(3), 1, 1, NE, 8));
     % split strain tensor into the parts for each integration point
     % infitismal (linear) strain tensor
-    epsilon = 0.5*(d0U+pagetranspose(d0U));
-    epsilon_int = 0.5*(d0U_int+pagetranspose(d0U_int));
+    epsilon = 0.5*(d0U+permute(d0U,[2 1 3 4]));
+    epsilon_int = 0.5*(d0U_int+permute(d0U_int, [2 1 3 4]));
     %% nonlinear displacement-strain matrix eq. 3.142
     BN = [temp.*repmat(FT_int, 1, 8, 1, 1); ...
         temp.*repmat(circshift(FT_int,[-1 0 0 0]), 1, 8, 1, 1)...
         + circshift(temp,[-1 0 0 0]).*repmat(FT_int, 1, 8, 1, 1)];
-    BNT = pagetranspose(BN);
+    BNT = permute(BN,[2 1 3 4]);
         %% Compute stress
     % assumption of plane stress
     tr = repmat(eye(3), 1, 1, NE, 8);
@@ -52,8 +52,8 @@ function [FORCE, GKF] = assembly(DISPTD, xPhys, penal, beta, eta, NE, NEQ, NDOF,
     gstiff = DET*sum(pagemtimes(pagemtimes(BNT, repmat(ETAN,1,1,1,8)),BN) ...
         + pagemtimes(pagemtimes(BG, 'transpose', SHEAD, 'none'),BG),4);
     %% assemble internal force vector and stiffness matrix     
-    FORCE = fsparse(reshape(pagetranspose(GDOF), 24*NE, 1), 1, ...
-        - reshape(pagetranspose(gforce), 24*NE, 1), [NEQ 1]);
+    FORCE = fsparse(reshape(permute(GDOF,[2 1 3 4]), 24*NE, 1), 1, ...
+        - reshape(permute(gforce,[2 1 3 4]), 24*NE, 1), [NEQ 1]);
     GKF = fsparse(reshape(repmat(GDOF, 1,24)', 576*NE, 1), ...
         reshape(repmat(reshape(GDOF',1, 24*NE), 24, 1), 1 , 576*NE)', ...
         reshape(gstiff, 576*NE ,1), [NEQ NEQ]);
